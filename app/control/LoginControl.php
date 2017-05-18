c1ee51a747d785198c646bb14c04c24d1f273d16
unknown <liming@bjev.com.cn>
2017-05-18 17:27:42 +0800

    a

--- app/control/LoginControl.php
+++ app/control/LoginControl.php
@@ -4,20 +4,31 @@ namespace app\control;
 
 use app\model\UserModel;
 
-class LoginControl extends \core\Control
-{
+class LoginControl extends \core\Control {
 
 
     public function login($para)
     { 
-        $userModel=new UserModel();
-            $res=$userModel->select('user', ['name','mail','password'],
-                ['name'=>$para['name'],'password'=>$para['password']]);
-        if(count($res)>0){
-             $this->display('index.html');
-        }else {
+        if (empty($para)) {
             $this->display('login.html');
+        } else {
+            $res=$this->checkUser($para['name'],$para['password']);
+            if ($res) {
+                $this->display('index.html');
+                //header('Location: http://www.baidu.com/');
+                $_SESSION['userid']
+
+            } else {
+                $this->display('login.html');
+            }
         }
-        
+    }
+
+    function checkUser($name,$password){
+          $userModel = new UserModel();
+         $res = $userModel ->select('user', ['name', 'mail', 'password'],
+                ['name'=>$name, 'password'=>$password]);
+
+         return count($res)>0;
     }
 }
