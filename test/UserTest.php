d3138941f1e3a0623cdc92cd4c4283c17ae8a894
unknown <liming@bjev.com.cn>
2017-06-15 09:09:06 +0800

    add unit test framework

--- test/UserTest.php
+++ test/UserTest.php
@@ -1,32 +1,15 @@
 <?php
 
+require 'TestBase.php';
 
-
-define('SILVER', realpath(__DIR__) . '/');
-require '..\vendor\autoload.php';
-require '..\core\Function.php';
-require '..\core\Silver.php';
-require '..\core\Model.php';
-
-spl_autoload_register('core\Silver::load');
-
-//require '..\index.php';
 use PHPUnit\Framework\TestCase;
-
 
 class UserTest extends TestCase
 {
-    public function testExpectFooActualFoo()
-    {
-        //$this->expectOutputString('foo');
-      // $albums= \app\model\AlbumModel::instance()->getAlbumsByUserId(1);
-    
-        //p($albums);
-       
+    public function testAlbum()
+    {  
         $albums= \app\model\AlbumModel::instance()->getAlbumsByUserId(1);
+        print_r($albums);
     }
-
-   
-
     
 }
