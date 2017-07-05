56976756b6afa3652cd3d4ac5afa41264b7a068d
unknown <liming@bjev.com.cn>
2017-07-05 15:52:22 +0800

    modify menu id

--- app/model/MenuModel.php
+++ app/model/MenuModel.php
@@ -12,7 +12,7 @@ class MenuModel extends \core\Model
     public function getMenusByAuthIds($ids)
     {
         if (is_null(self::$_menus) || isset(self::$_menus)) {
-            self::$_menus = self::instance()->select('menu', '*');
+            self::$_menus = self::instance()->getAll();
         }
         $menusArray = explode(',', $ids);
 
@@ -24,37 +24,49 @@ class MenuModel extends \core\Model
     public function getTreeMenusByAuthIds($ids)
     {
         if (is_null(self::$_menus) || isset(self::$_menus)) {
-            self::$_menus = self::instance()->select('menu', '*');
+            self::$_menus = self::instance()->getAll();
         }
-
         $menusAuthIds = explode(',', $ids);
-     
-        $menusByAuth = array_uintersect(self::$_menus, $menusAuthIds, 'self::compareId');
 
-        $menusTree = array();
-        $this->setTreeMenu(0, $menusTree, $menusByAuth);
-         
-        return $menusTree['children'];
+        $menusByAuth = array_uintersect(self::$_menus, $menusAuthIds, function ($v1, $v2) {
+            if (is_array($v1)) {
+                $v1 = $v1['id'];
+            }
+            if (is_array($v2)) {
+                $v2 = $v2['id'];
+            }
+            if ((int) $v1 === (int) $v2) {
+                return 0;
+            } else {
+                return ((int) $v1 > (int) $v2) ? 1 : -1;
+            }
+
+        });
+        $menus = array();
+
+        $this->setTreeMenu(0, $menus, $menusByAuth);
+
+        return $menus['children'];
     }
 
     protected static $_flagParentId = null;
-    private function setTreeMenu($parentId, &$menuItem, $menusByAuth)
+    private function setTreeMenu($parentId, &$menusTree, $menusByAuth)
     {
         self::$_flagParentId = $parentId;
-       
-        $menuItem['children'] = array_filter($menusByAuth, function ($item) {
+
+        $menusTree['children'] = array_filter($menusByAuth, function ($item) {
             if ($item['parent_id'] == self::$_flagParentId) {
                 return true;
             }
         });
-        $menuItem['hasChildren'] = count($menuItem['children']) > 0 ? true : false;
-        foreach ($menuItem['children'] as $key => $value) {
-            $this->setTreeMenu($menuItem['children'][$key]['id'], $menuItem['children'][$key], $menusByAuth);
+        $menusTree['hasChildren'] = count($menusTree['children']) > 0 ? true : false;
+        foreach ($menusTree['children'] as $key => $value) {
+            $this->setTreeMenu($menusTree['children'][$key]['id'], $menusTree['children'][$key], $menusByAuth);
         }
     }
 
     private static function compareId($v1, $v2)
-    {  
+    {
         if (is_array($v1)) {
             $v1 = $v1['id'];
         }
@@ -63,8 +75,9 @@ class MenuModel extends \core\Model
         }
         if ((int) $v1 === (int) $v2) {
             return 0;
+        } else {
+            return ((int) $v1 > (int) $v2) ? 1 : -1;
         }
-        return 1;
     }
 
 }
